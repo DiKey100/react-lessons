d724466bf52222c5797ea35e2439a19a3e877fbf
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-03 01:16:25 +0200

    addJsxVersionAndWithoutJsxVersionCode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// const title = React.createElement('h1', { id: 'title' }, 'Hello Reacct')
+const title = <h1 id="title">Hello React 2</h1>
+console.log(title)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{title}</React.StrictMode>)
