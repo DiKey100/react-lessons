9cbfb706b11054fc1d24d452cb8ffaf04733eb9a
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-03 03:00:03 +0200

    addComponentJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// const title = React.createElement('h1', { id: 'title' }, 'Hello Reacct')
-const title = <h1 id="title">Hello React 2</h1>
-console.log(title)
+function App() {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+                repudiandae iusto tempore maxime molestias iure id officia odit
+                perspiciatis adipisci, provident, nemo pariatur quisquam
+                exercitationem fugit? Quia blanditiis incidunt explicabo.
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
