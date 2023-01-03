0db9fc0a2d65edb5d4f3d8f1354794b1b8cbffb1
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-03 17:45:57 +0200

    addAbsoluteImports

--- src/Title.tsx
+++
@@ -1,9 +0,0 @@
-type TitleProps = {
-    title: string | number
-}
-
-export const Title = (props: TitleProps) => {
-    return <h1>Hello {props.title}</h1>
-}
-
-// export default Title
--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Title } from './Title'
-
-type ContentProps = {
-    p1: string
-    p2: string
-}
-const Content = (props: ContentProps) => {
-    return (
-        <React.Fragment>
-            <p>{props.p1}</p>
-            <p>{props.p2}</p>
-        </React.Fragment>
-    )
-}
-const App = () => {
-    return (
-        <>
-            <Title title="React" />
-            <Title title="TS" />
-            <Title title="JS" />
-            <Content p1="Hello world. How're you?" p2="It's my love black mf" />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
