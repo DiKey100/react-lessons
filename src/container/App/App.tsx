e4ec9f3929227e47afcdd2fba1b4a25699af5e68
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-03 18:22:57 +0200

    add App and Header

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,5 +1,11 @@
+import Header from 'container/Header/Header'
+
 type Props = {}
 const App = (props: Props) => {
-    return <div>App</div>
+    return (
+        <>
+            <Header />
+        </>
+    )
 }
 export default App
