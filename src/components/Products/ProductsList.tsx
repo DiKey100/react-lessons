f03bc4bb6ff2f84a58e026baba293569f9f77be2
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-06 05:19:11 +0200

    MyVariantAddingInfoInSite

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -13,13 +13,28 @@ const ProductsList = (props: Props) => {
                 spacing={4}
             >
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone X"
+                        type="phone"
+                        capacity="64Gb"
+                        price="400$"
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 14 Pro"
+                        type="phone"
+                        capacity="1Tb"
+                        price="2000$"
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 12"
+                        type="phone"
+                        capacity="128Gb"
+                        price="1000$"
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -1,20 +1,26 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
+import './ProductsListItem.scss'
 
-type Props = {}
+type Props = {
+    title: string
+    type: string
+    capacity: number | string
+    price: number | string
+}
 const ProductsListItem = (props: Props) => {
     return (
         <Card className="product" variant="outlined">
             <CardContent>
-                <div className="product-title">iPhone X</div>
-                <div className="product-decs">This's iPhone X</div>
+                <div className="product-title">{props.title}</div>
+                <div className="product-decs">This's {props.title}</div>
                 <div className="product-features">
-                    <span>Type:</span> phone
+                    <span>Type:</span> {props.type}
                 </div>
                 <div className="product-features">
-                    <span>Capacity:</span> 64Gb
+                    <span>Capacity:</span> {props.capacity}
                 </div>
                 <div className="product-price">
-                    <span>Price:</span> 500$
+                    <span>Price:</span> {props.price}
                 </div>
             </CardContent>
             <CardActions className="btns-wrap">
