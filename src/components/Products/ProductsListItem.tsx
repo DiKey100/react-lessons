f9388f1ea029236e361e48a762483e62cd931a93
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-06 02:48:18 +0200

    addScssInProductFileAndMarkuo

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -3,16 +3,22 @@ import { Button, Card, CardActions, CardContent } from '@mui/material'
 type Props = {}
 const ProductsListItem = (props: Props) => {
     return (
-        <Card className="product">
+        <Card className="product" variant="outlined">
             <CardContent>
                 <div className="product-title">iPhone X</div>
-                <div className="product-dec">This's iPhone X</div>
-                <div className="product-features">Type: phone</div>
-                <div className="product-features">Capacity: 64Gb</div>
-                <div className="product-price">500$</div>
+                <div className="product-decs">This's iPhone X</div>
+                <div className="product-features">
+                    <span>Type:</span> phone
+                </div>
+                <div className="product-features">
+                    <span>Capacity:</span> 64Gb
+                </div>
+                <div className="product-price">
+                    <span>Price:</span> 500$
+                </div>
             </CardContent>
-            <CardActions>
-                <Button>Add to cart</Button>
+            <CardActions className="btns-wrap">
+                <Button variant="outlined">Add to cart</Button>
             </CardActions>
         </Card>
     )
