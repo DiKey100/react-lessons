663c7e9aefdb35d4f393e3ecd4f594e57c26805b
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-17 00:01:16 +0200

    fixErrorCodeInApp.tsx

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -14,14 +14,10 @@ const App = (props: Props) => {
         totalCount: 0,
         totalPrice: 0,
     })
-
-    const addProductToCart = () => {}
-
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
-            <button onClick={() => addProductToCart()}></button>
             <Main />
         </StyledEngineProvider>
     )
