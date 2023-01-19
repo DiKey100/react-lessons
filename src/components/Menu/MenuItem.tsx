63e4ed5d73ae821f5a7bf02b38fe4a7c058cfa36
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-01-19 08:25:34 +0200

    addNewLibraryAndLinkToMenuWithoutStyle

---
+++ src/components/Menu/MenuItem.tsx
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button'
+import { NavLink } from 'react-router-dom'
+
+type Props = {
+    to: string
+    children: React.ReactNode
+}
+
+const MenuItem = ({ to, children }: Props) => {
+    return (
+        <Button color="inherit">
+            <NavLink to={to}>{children}</NavLink>
+        </Button>
+    )
+}
+export default MenuItem
